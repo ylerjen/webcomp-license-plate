dfc705218cffbe99e559d43eac462b39761c8acf
Lerjen Yann <ylerjen@vaudoise.ch>
2018-12-07 19:12:03 +0100

    develop add plugin-proposal-class-properties

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,14 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env']
+                        presets: [
+                            '@babel/preset-env',
+                            {
+                                plugins: [
+                                    '@babel/plugin-proposal-class-properties'
+                                ]
+                            }
+                        ]
                     }
                 }
             }, {
