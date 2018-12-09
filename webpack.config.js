dc0a601024ff64aa48422bb4fb48ea8daa9d39fb
Yann Lerjen <yann.lerjen@gmail.com>
2018-12-09 21:52:22 +0100

    add png loader

--- webpack.config.js
+++ webpack.config.js
@@ -63,6 +63,18 @@ module.exports = {
                         }           
                     }
                 ]
+            }, {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                        bypassOnDebug: true, // webpack@1.x
+                        disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ]
             }
         ]
     }
